7f934dabf478f2e7c2cb0a63fddf0a26531c2868
davidgonzalezfx <dvdramos16@gmail.com>
2020-07-16 19:57:19 +0000

    Fix - Calculator 2.0

--- cohort12/0x0F-function_pointers/calculator.c
+++ cohort12/0x0F-function_pointers/calculator.c
@@ -1,38 +1,118 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int add(int numerOne, int numerTwo)
+typedef struct op
 {
-    return numerOne + numerTwo;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+/**
+ * op_add - add a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a with b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a module b
+ * @a: int
+ * @b: int
+ * Return: void
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
 
-int subtract(int numerOne, int numerTwo)
+int (*get_op_func(char *s))(int, int)
 {
-    return numerOne - numerTwo;
-}
+	op_t ops[] = {
+	    {"+", op_add},
+	    {"-", op_sub},
+	    {"*", op_mul},
+	    {"/", op_div},
+	    {"%", op_mod}
+	};
 
-int multiply(int numerOne, int numerTwo)
-{
-    return numerOne * numerTwo;
-}
+	int i = 0;
 
-int divide(int numerOne, int numerTwo)
-{
-    return numerOne / numerTwo;
+	while (i < 5)
+	{
+		if (*s == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 int main(int argc, char *argv[])
 {
-    printf("cantidad de parametros: %d\n", argc);
+	int num1 = 0, num2 = 0, res = 0;
 
-    int i = 0;
-    for (i = 1; argv[i - 1] != '\0'; i++)
-    {
-        printf("parametro numero: %d - %s\n", i, argv[i - 1]);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    int (*arrCalculatorFunctions[])(int, int) = {add, subtract, multiply, divide};
+	if ((argv[2][0] != '+' &&
+	    argv[2][0] != '-' &&
+	    argv[2][0] != '*' &&
+	    argv[2][0] != '/' &&
+	    argv[2][0] != '%') || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d", arrCalculatorFunctions[0](4, 4));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    return 0;
+	if (num2 == 0 &&
+	(argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", res);
+
+	return (0);
 }
